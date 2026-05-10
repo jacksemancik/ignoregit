8ca34d5a4601e16384cb02bc36332895eea4d1e7
Jack Semancik <jack.semancik@yahoo.com>
2026-05-09 21:39:15 -0700

    Added ignoregit.sh file

---
+++ ignoregit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ignoregit.sh, or Ignore-Git!
+# Author: Jack Semancik
+# Email: jack.semancik@yahoo.com
+# License: GNU General Public License v3
+# Description: A simple utility for placing a template .gitignore file into the current working directory.
+#
+
+cp ~/templates/gitignore-template ./.gitignore
